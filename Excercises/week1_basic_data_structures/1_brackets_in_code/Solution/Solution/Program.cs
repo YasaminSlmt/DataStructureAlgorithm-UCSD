ba1676be5d418df20e7b9f15d830501d5633a5d1
Yasamin Salamat <y.salamat@bymmt.com>
2022-03-29 21:53:39 -0400

    Add project files.

---
+++ Excercises/week1_basic_data_structures/1_brackets_in_code/Solution/Solution/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         //char[] inputTxt = File.ReadAllText(@"G:\My Drive\Yasamin\Courses\Data Structure and Algorithm Specialization\Excercises\week1_basic_data_structures\1_brackets_in_code\tests\26").ToCharArray();
+
+
+         char[] inputTxt = Console.ReadLine().ToCharArray();
+
+
+         Stack brackets = new Stack();
+         bool result = true;
+         IDictionary<char, char> pairs = new Dictionary<char, char>()
+            {
+                { '(',')'},
+                { '[',']'},
+                {'{','}' }
+            };
+
+        int badIdx = 0;
+        for (int idx = 0; idx < inputTxt.Length; idx++) 
+            {
+                if (!pairs.ContainsKey(inputTxt[idx]) && !pairs.Values.Contains(inputTxt[idx]))
+                {
+                    continue;
+                }
+
+            if (pairs.Any(current => inputTxt[idx].Equals(current.Key)))
+                    brackets.Push(inputTxt[idx]);
+            else if (brackets.Count == 0)
+                    {
+                        result = false;
+                        badIdx = idx + 1;
+                        break;
+                    }
+            else
+                    {
+                        char lastIn = (Char)brackets.Pop();
+                        if (!inputTxt[idx].Equals(pairs[lastIn]))
+                        {
+                            result = false;
+                            badIdx = idx + 1;
+                            break;
+                        }
+
+                    }
+
+
+
+            }
+                // Check if the stack is empty
+             if (brackets.Count != 0 && result == true)
+                {
+                    result = false;
+                    badIdx = inputTxt.Length;
+                }
+                    
+
+             Console.WriteLine((result == true ? "Success" : badIdx)); 
+           
+
+
+
+
+        }
+    }
+}
---
+++ Excercises/week1_basic_data_structures/2_tree_height/Solution/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32 (Console.ReadLine());
+            string[] inputTxt = File.ReadAllLines(@"G:\My Drive\Yasamin\Courses\Data Structure and Algorithm Specialization\Excercises\week1_basic_data_structures\2_tree_height\tests\24");
+            string[] inputStr = inputTxt[1].Split(" ");
+            //string[] inputStr = Console.ReadLine().Split(" ");
+            int[] input = Array.ConvertAll(inputStr, s => int.Parse(s));
+
+            int maxHeight = 0;
+            foreach(int node in input)
+            {
+                int height = 0;
+                int current = node;
+                while(current != -1)
+                {
+                    height++;
+                    current = input[current];
+                }
+                maxHeight = Math.Max(maxHeight, height + 1);
+            }
+
+            Console.WriteLine(maxHeight);
+
+            
+        }
+    }
+}
