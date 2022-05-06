871218628c43d44d705496ae795cc96fca91f091
Yasamin Salamat <y.salamat@bymmt.com>
2022-05-05 21:54:42 -0400

    Start file

---
+++ Excercises/week2_priority_queues_and_disjoint_sets/2_job_queue/Solution/Solution/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Solution
+{
+    public class Thread
+    {
+        public bool isFree { get; set; }
+        public int currentJob { get; set; }
+        public int timeUntilFree { get; set; }
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputStr1 = Console.ReadLine().Split(" ");
+            int nThreads = Convert.ToInt32(inputStr1[0]);
+            int nJobs = Convert.ToInt32(inputStr1[1]);
+
+            string[] inputStr2 = Console.ReadLine().Split(" ");
+            int[] inputNums = Array.ConvertAll(inputStr2, s => int.Parse(s));
+
+
+
+        }
+    }
+}
