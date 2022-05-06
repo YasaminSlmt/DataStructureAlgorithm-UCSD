522aa036e52773c0868b867e099ea6c79e7cf80d
Yasamin Salamat <y.salamat@bymmt.com>
2022-05-05 20:36:03 -0400

    BuildHeap Solution

--- Excercises/week2_priority_queues_and_disjoint_sets/1_make_heap/Solution/Program.cs
+++ Excercises/week2_priority_queues_and_disjoint_sets/1_make_heap/Solution/Program.cs
@@ -11,20 +11,21 @@ namespace Solution
     {
         public List<int> Data = new List<int>();
         public SwapArray swap = new SwapArray();
+        public int Size = new int();
 
         public int Parent (int i)
         {
-            return (int)Math.Floor(Convert.ToDouble(i)/ 2.0);
+            return (int)Math.Floor(Convert.ToDouble(i-1)/ 2.0);
         }
 
         public int LeftChild(int i)
         {
-            return 2*i;
+            return 2*i+1;
         }
 
         public int RightChild(int i)
         {
-            return 2*i+1;
+            return 2*i+2;
         }
 
         public void Swap(int index1, int index2)
@@ -39,7 +40,7 @@ namespace Solution
 
         public void SiftUp(int i)
         {
-            while (i > 0 && Data[Parent(i)] < Data[i])
+            while (i > 0 && Data[Parent(i)] > Data[i])
             {
                 Swap(Parent(i), i);
                 i = Parent(i);
@@ -48,27 +49,28 @@ namespace Solution
 
         public void SiftDown(int i)
         {
-            int maxIndex = i;
+            int minIndex = i;
             int leftChild = LeftChild(i);
 
-            if (leftChild <= Data.Count() && Data[leftChild] > Data[maxIndex])
-                maxIndex = leftChild;
+            if (leftChild < Size && Data[leftChild] < Data[minIndex])
+                minIndex = leftChild;
 
             int rightChild = RightChild(i);
-            if (rightChild <= Data.Count() && Data[rightChild] > Data[maxIndex])
-                maxIndex = rightChild;
+            if (rightChild < Size && Data[rightChild] < Data[minIndex])
+                minIndex = rightChild;
 
-            if (i != maxIndex)
+            if (i != minIndex)
             {
-                Swap(i, maxIndex);
-                SiftDown(maxIndex);
+                Swap(i, minIndex);
+                SiftDown(minIndex);
             }
         }
 
-        public int ExtractMax()
+        public int ExtractMin()
         {
             int result = Data[0];
-            Data[0] = Data[Data.Count() - 1];
+            Data[0] = Data[Size - 1];
+            Size -= 1 ;
             SiftDown(0);
             return result;
         }
@@ -77,8 +79,10 @@ namespace Solution
     public class SwapArray
     {
         
-        public List<int> Index1 { get; set; }
-        public List<int> Index2 { get; set; }
+        public List<int> Index1 = new List<int>();
+        public List<int> Index2 = new List<int>();
+
+
     }
     
     
@@ -91,12 +95,35 @@ namespace Solution
             int[] inputNums = Array.ConvertAll(inputStr, s => int.Parse(s));
 
             Heap heap = new Heap();
+            heap.Size = n;
 
             for (int i = 0; i < n; i++)
             {
                 heap.Data.Add(inputNums[i]);
             }
-            int stop = 1;
+            // Build the heap
+            for (int i = (int)Math.Floor(Convert.ToDouble(heap.Size) / 2.0); i>=0 ; i--)
+            {
+                heap.SiftDown(i);
+            }
+
+            /// For sorting
+            //for (int i = 0; i <n; i++)
+            //{
+            //    heap.Swap(0,i);
+            //    heap.Size = i;
+            //    heap.SiftDown(0);
+            //}
+
+            // Printing
+            Console.WriteLine(heap.swap.Index1.Count());
+            for(int i = 0; i < heap.swap.Index1.Count(); i++)
+            {
+                Console.WriteLine(heap.swap.Index1[i] + " " + heap.swap.Index2[i]);
+            }
+
+                
+
         }
     }
 }
